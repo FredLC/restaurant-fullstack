75817b398296cf75c15b94bfbd01ba4ae5a0b861
fredlc <fredlefevre@outlook.com>
2020-04-25 14:59:22 -0400

    Started to make website responsive

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="welcome-jumbotron">
-      <h1>About</h1>
+    <div id="about-page">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1>About Us</h1>
+            <p>I'm baby cliche mixtape pickled mustache. Cronut pug succulents lyft stumptown kickstarter pok pok semiotics photo booth disrupt master cleanse asymmetrical coloring book banh mi cred. Raclette synth banh mi fanny pack pug 90's, direct trade pitchfork hot chicken mumblecore kale chips before they sold out distillery disrupt. Semiotics lomo green juice freegan pug meggings kombucha twee kale chips hammock. Shaman XOXO kinfolk lomo hoodie tote bag prism, messenger bag freegan. Man braid authentic pinterest chartreuse PBR&B, migas biodiesel next level. Af chicharrones organic, small batch meditation vice +1 swag kitsch brooklyn vape.</p>
+          </div>
+          <div class="col-md-6">
+            <img src="img/chef.jpeg" alt="about page" class="img-fluid">
+          </div>
+        </div>
+      </div>
     </div>
 @endsection
--- resources/views/pages/giftcards.blade.php
+++ resources/views/pages/giftcards.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="welcome-jumbotron">
+    <div class="">
       <h1>Gift Cards</h1>
     </div>
 @endsection
