5f33e66c7d1e3d12db1a387de6d00dcef6ac3a00
fredlc <fredlefevre@outlook.com>
2020-05-08 12:45:06 -0400

    Styled home and about page to be responsive

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -8,7 +8,7 @@
             <h1>About Us</h1>
             <p>I'm baby cliche mixtape pickled mustache. Cronut pug succulents lyft stumptown kickstarter pok pok semiotics photo booth disrupt master cleanse asymmetrical coloring book banh mi cred. Raclette synth banh mi fanny pack pug 90's, direct trade pitchfork hot chicken mumblecore kale chips before they sold out distillery disrupt. Semiotics lomo green juice freegan pug meggings kombucha twee kale chips hammock. Shaman XOXO kinfolk lomo hoodie tote bag prism, messenger bag freegan. Man braid authentic pinterest chartreuse PBR&B, migas biodiesel next level. Af chicharrones organic, small batch meditation vice +1 swag kitsch brooklyn vape.</p>
           </div>
-          <div class="col-md-6">
+          <div class="col-md-6 image">
             <img src="img/chef.jpeg" alt="about page" class="img-fluid">
           </div>
         </div>
