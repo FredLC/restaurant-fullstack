e151411b4d54b1b1811c4c599757d9957cec1d69
fredlc <fredlefevre@outlook.com>
2020-05-08 19:22:38 -0400

    Added admin login and register pages

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,14 @@ Route::get('/admin', function () {
     return view('admin/dashboard');
 });
 
+Route::get('/admin/register', function () {
+    return view('admin/register');
+});
+
+Route::get('/admin/login', function () {
+    return view('admin/login');
+});
+
 Route::get('/menu/{slug}', function () {
     return view('menu/single-menu');
 });
