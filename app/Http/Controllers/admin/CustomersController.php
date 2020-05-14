5742a0bc362dee8e2a33ef28ae0db8b52315e525
fredlc <fredlefevre@outlook.com>
2020-05-14 15:06:03 -0400

    Final fixes for authentication

--- app/Http/Controllers/admin/CustomersController.php
+++ app/Http/Controllers/admin/CustomersController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class CustomersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function subscribers() {
         return view('admin/customers/subscribers');
     }
